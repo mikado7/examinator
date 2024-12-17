44622190778c491c5486a9dfaa6bdd765fb9a5ef
mikado7 <borovy0@gmail.com>
2024-12-17 13:00:12 +0100

    refactor to hash router

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import App from './App.vue'
 import router from './router/router'
 import Quill from 'quill'
 import FormulaCustom from './quill/formula_custom'
-import {addExampleData} from "@/db/db";
 
 Quill.register(FormulaCustom, true)
 
